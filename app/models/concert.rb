5a0cdb095e9110d12ec31d63f34c47a8f28e3966
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-03-06 20:50:42 +0100

    added the has_many and belongs_to to the models. Please double check this as im not too sure

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -1,2 +1,4 @@
 class Concert < ApplicationRecord
+  belongs_to :artist
+  has_many :saved_concerts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
+
+  has_many :saved_concerts
+  has_many :followed_artists
 end
