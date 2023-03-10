59703d02cada4c4ac44f9280fc0b0d73092de01f
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-10 22:13:07 +0000

    added the action and view to see all concerts + aplication_policy.rb

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,2 +1,7 @@
 class ConcertsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  def index
+    @concerts = policy_scope(Concert)
+    @concerts = Concert.all
+  end
 end
