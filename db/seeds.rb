81a1b1395bcdb045a5b151dff68700fe36af22e7
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-10 22:17:16 +0000

    removed my seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-
-
-ana = Artist.new(name: "ana", description: "Monteiro")
-carolina = Artist.new(name: "carolina", description: "Monteiro")
-
-
-Concert.create!(location: "lisbon", description: "Great show", date: DateTime.new(2009,9,1,17), price: 5, ticket_url: "www.hts", artist: ana)
-Concert.create!(location: "azores", description: "Jazz", date: DateTime.new(2009,9,1,17), price: 2, ticket_url: "www.hts", artist: carolina)
-Concert.create!(location: "faro", description: "Blues", date: DateTime.new(2009,9,1,17), price: 5, ticket_url: "www.hts", artist: ana)
-
-
-
-# t.string "location"
-# t.text "description"
-# t.date "date"
-# t.float "price"
-# t.string "ticket_url"
