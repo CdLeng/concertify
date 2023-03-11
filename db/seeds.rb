0fb3ee428a576a991dd08d06a7405bc16c968350
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-11 16:10:39 +0000

    added the artist#index action to see the page of all artists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ana = Artist.new(name: "ana", description: "Monteiro")
+carolina = Artist.new(name: "carolina", description: "Monteiro")
