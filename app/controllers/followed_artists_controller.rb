ed69a134463ac95aae20cd693be06ec1d182027a
Isaac Lichtenstein <islichtenstein@gmail.com>
2023-03-11 21:10:21 +0100

    added the create method for followed_artists. on the artists page there is a button at the top to follow the artist.

--- app/controllers/followed_artists_controller.rb
+++ app/controllers/followed_artists_controller.rb
@@ -3,4 +3,17 @@ class FollowedArtistsController < ApplicationController
     @followed_artists = policy_scope(FollowedArtist)
     @followed_artists = FollowedArtist.all
   end
+
+  def create
+    @followed_artist = FollowedArtist.new
+    @followed_artist.artist = Artist.find(params[:artist_id])
+    @followed_artist.user = current_user
+    authorize @followed_artist
+
+    if @followed_artist.save!
+      redirect_to followed_artists_path, notice: "This artist was added to favourites."
+    else
+      render "artists/show", status: :unprocessable_entity
+    end
+  end
 end
