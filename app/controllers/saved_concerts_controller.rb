e1110bbaa674cbadf707f5a7a73700b8d28b6773
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-11 17:20:57 +0000

    corrected the params in the saved_concerts#create

--- app/controllers/saved_concerts_controller.rb
+++ app/controllers/saved_concerts_controller.rb
@@ -2,7 +2,7 @@ class SavedConcertsController < ApplicationController
 
   def create
     authorize @saved_concert
-    @saved_concert = SavedConcert.create!(params[:concert_id])
+    @saved_concert = SavedConcert.create!(params[:id])
 
     if @saved_concert.save!
       redirect_to saved_concert_path(@saved_concert), notice: "This concert was succesfully saved."
