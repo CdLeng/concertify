212f0fabe6e61987f0e8bad53d38b83a87e8c54a
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-11 16:12:15 +0000

    removed some dummy seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-ana = Artist.new(name: "ana", description: "Monteiro")
-carolina = Artist.new(name: "carolina", description: "Monteiro")
