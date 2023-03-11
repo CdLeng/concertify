bdef300b7c534bd3087ea8b0bdbe25b913297b82
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-11 15:38:25 +0000

    added save concert button to concerts#show page (dont know if works, i need to update master first)

--- app/controllers/saved_concerts_controller.rb
+++ app/controllers/saved_concerts_controller.rb
@@ -1,2 +1,13 @@
 class SavedConcertsController < ApplicationController
+
+  def create
+    authorize @saved_concert
+    @saved_concert = SavedConcert.create!(params[:concert_id])
+
+    if @saved_concert.save!
+      redirect_to saved_concert_path(@saved_concert), notice: "This concert was succesfully saved."
+    else
+      render "concerts/show", status: :unprocessable_entity
+    end
+  end
 end
