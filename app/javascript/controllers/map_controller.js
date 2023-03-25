b4b9bb4dee100c2d959b79e07bd7f8ca9fe42c7c
Carolina Monteiro <carolinaviveirosmonteiro@gmail.com>
2023-03-25 14:40:05 +0000

    .

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -12,11 +12,12 @@ export default class extends Controller {
     mapboxgl.accessToken = this.apiKeyValue;
     this.map = new mapboxgl.Map({
       container: this.element, // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      style: 'mapbox://styles/carolinavmo/clfo2eda600e101qiadzfb9l2', // style URL
     });
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+
   }
 
   #addMarkersToMap() {
@@ -32,6 +33,6 @@ export default class extends Controller {
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-    this.map.fitBounds(bounds, { padding: 110, maxZoom: 15, duration: 0 })
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
